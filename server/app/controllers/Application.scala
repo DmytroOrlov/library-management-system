12eeee6c80c50172c8afdb9877b4e9c18f81e090
Dmytro Orlov <orlov.dm@gmail.com>
2016-01-20 14:13:19 +0100

    misc

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -6,13 +6,13 @@ import play.api.mvc._
 
 class Application extends Controller {
   def index = Action { implicit request =>
-    request.session.get(username).fold(Redirect(routes.UserController.register)) { _ =>
+    request.session.get(username).fold(Redirect(routes.UserController.getRegister)) { _ =>
       Ok(views.html.index())
     }
   }
 
   def logout = Action { implicit request =>
-    request.session.get(username).fold(Redirect(routes.UserController.register).withNewSession) { _ =>
+    request.session.get(username).fold(Redirect(routes.UserController.getRegister).withNewSession) { _ =>
       Redirect(routes.UserController.login)
         .withNewSession
         .flashing(flashToUser -> logoutDone)
