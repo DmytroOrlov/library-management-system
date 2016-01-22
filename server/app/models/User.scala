905770d1888027d4ceb0b02a49185071127cb31e
Dmytro Orlov <orlov.dm@gmail.com>
2016-01-22 18:03:41 +0100

    user: change writeable

--- server/app/models/User.scala
+++ server/app/models/User.scala
@@ -6,7 +6,9 @@ import play.api.libs.json._
 case class User(name: String, password: String, id: Option[Int] = None)
 
 object User {
-  implicit val userFormat = Json.format[User]
+  implicit val format = Json.format[User]
 
-  implicit def userWriteable(implicit w: Writeable[JsValue]): Writeable[User] = w.map(Json.writes[User].writes)
+  implicit val writes = Json.writes[User]
+
+  implicit def writable(implicit w: Writeable[JsValue]): Writeable[User] = w.map(writes.writes)
 }
