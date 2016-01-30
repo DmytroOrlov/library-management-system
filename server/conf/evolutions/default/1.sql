2f693f3257e360595d83d1d517b46ced9ac8ac50
Dmytro Orlov <orlov.dm@gmail.com>
2016-01-30 15:03:34 +0100

    db: add visitor, new_visitor

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -1,11 +1,25 @@
 # --- !Ups
 
+CREATE TABLE "visitor" (
+  "id" SERIAL PRIMARY KEY
+);
+
 CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY,
   "name" VARCHAR NOT NULL UNIQUE,
-  "password" VARCHAR NOT NULL
+  "password" VARCHAR NOT NULL,
+  "visitor_id" INTEGER UNIQUE REFERENCES "visitor"("id")
 );
+
+CREATE TABLE "new_visitor" (
+  "id" INTEGER NOT NULL UNIQUE REFERENCES "user"
+);
+
 
 # --- !Downs
 
 DROP TABLE "user" IF EXISTS;
+
+DROP TABLE "visitor" IF EXISTS;
+
+DROP TABLE "new_visitor" IF EXISTS;
