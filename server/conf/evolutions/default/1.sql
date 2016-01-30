3ad6a47256d0cb5f26fc7b04ba25d07c23d868e9
Dmytro Orlov <orlov.dm@gmail.com>
2016-01-30 15:57:55 +0100

    db: add library

--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -1,7 +1,14 @@
 # --- !Ups
 
+CREATE TABLE "library" (
+  "uuid" UUID PRIMARY KEY
+  , "name" VARCHAR NOT NULL UNIQUE
+);
+
 CREATE TABLE "visitor" (
-  "id" SERIAL PRIMARY KEY
+  "id" INTEGER NOT NULL,
+  "library_uuid" UUID NOT NULL REFERENCES "library"("uuid"),
+  PRIMARY KEY ("id", "library_uuid")
 );
 
 CREATE TABLE "user" (
@@ -18,8 +25,10 @@ CREATE TABLE "new_visitor" (
 
 # --- !Downs
 
-DROP TABLE "user" IF EXISTS;
+DROP TABLE "library" IF EXISTS;
 
 DROP TABLE "visitor" IF EXISTS;
+
+DROP TABLE "user" IF EXISTS;
 
 DROP TABLE "new_visitor" IF EXISTS;
