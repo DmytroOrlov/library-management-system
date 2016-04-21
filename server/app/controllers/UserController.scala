618a7143c028799c431b99279020c1d7a736dc27
Dmytro Orlov <orlov.dm@gmail.com>
2016-04-21 23:07:55 +0200

    Google: add GoogleController

--- server/app/controllers/UserController.scala
+++ server/app/controllers/UserController.scala
@@ -39,32 +39,6 @@ class UserController @Inject()(repo: UserRepository, val messagesApi: MessagesAp
     )(LoginForm.apply)(LoginForm.unapply)
   }
 
-  val clientId = config.getString("google.clientId")
-  val clientSecret = config.getString("google.clientSecret")
-
-  def withGoogle = Action {
-    val secretState = "secret" + Random.nextInt(999999)
-    val service = new ServiceBuilder()
-      .apiKey(clientId)
-      .apiSecret(clientSecret)
-      .scope("email")
-      .state(secretState)
-      .callback("http://bibliman.com:9000/oauth2callback")
-      .build(GoogleApi20.instance)
-    val authorizationUrl = {
-      import scala.collection.JavaConversions._
-      service.getAuthorizationUrl(Map("access_type" -> "offline", "prompt" -> "consent"))
-    }
-    SeeOther(authorizationUrl)
-      .withSession("secretState" -> secretState)
-  }
-
-  def oauth2callback(state: String, code: String) = Action { implicit r =>
-    r.session.get("secretState").filter(_ == state).fold(BadRequest("bad")) { _ =>
-      Ok("ok").removingFromSession("secretState")
-    }
-  }
-
   def getLogin = Action { implicit request =>
     request.session.get(useruuid).fold(Ok(views.html.login(loginForm))) { _ =>
       Redirect(routes.Application.index)
