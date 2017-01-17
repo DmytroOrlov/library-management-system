0ae20b978cecdd94ecf45e2c6a8da9c5fb42c706
Dmytro Orlov <orlov.dm@gmail.com>
2017-01-17 16:17:56 +0100

    guice: move Module

--- server/app/Module.scala
+++ server/app/Module.scala
@@ -1,11 +1,9 @@
-package config
-
-import com.google.inject.{Singleton, Provides, AbstractModule}
+import com.google.inject.{AbstractModule, Provides, Singleton}
 import com.typesafe.config.Config
 import play.api.Configuration
 
-class LmsModule extends AbstractModule {
-  def configure() = {}
+class Module extends AbstractModule {
+  def configure() = ()
 
   @Provides @Singleton
   def config(c: Configuration): Config = c.underlying
